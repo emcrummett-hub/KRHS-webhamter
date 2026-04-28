09dcc07fe50e6197deea5aac454adc723a95fe3d
emcrummett-hub <emcrummett@gmail.com>
2026-04-27 20:21:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (let o = 0; o < dots.length; o++) {
+    dots[o].className = dots[o].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
